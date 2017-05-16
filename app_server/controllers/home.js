eb89ecb25d601ea4635be19bd68e8b04d2e938c0
Michael Pascuzzi <gatorpazz@gmail.com>
2017-05-15 23:53:51 -0400

    Moved data out of views into controller

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -1,3 +1,7 @@
 module.exports.home = function(req, res){
-  res.render('index', { title: 'Knights of Columbus Council 12402' });
+  res.render('index', {
+    title: 'Knights of Columbus Council 12402',
+    knightOfTheMonth: "Knight of the month for March was awarded to Brother Raul Rodriguez. Brother Raul participated in the 1st fish fry on Friday, Habitat for Humanity on Saturday, and Fed the Homeless on Monday – all over 72 hours. Brother Raul joined our council in Sep 2016 as a 1st degree and made his 2nd, 3rd, and 4th degrees by Nov 2016. Congrats on this well deserved award.",
+    familyOfTheMonth: "Family of the month for March was awarded to Mark and Liz Zeitler. Brother Mark and family have been engaged in a number of activities over the past month starting with the St. Patrick’s Day Dinner, Laps for Life, and our 1st fish fry. Brother Mark & Liz participated in Cursillo and Mark did a stellar job as the lead for That Man Is You. All are also very active at St. James School. Thanks to the Zeitler family for the tremendous example that they are setting and congrats on this well deserved award."
+  });
 };
--- app_server/controllers/officers.js
+++ app_server/controllers/officers.js
@@ -1,3 +1,66 @@
 module.exports.officers = function(req, res){
-  res.render('officers', { title: 'Council Officers' });
+  res.render('officers', {
+    title: 'Council Officers',
+    officers: [{
+      title: 'Chaplain',
+      name: 'Fr. Martin Nguyen',
+      picture: 'chaplain.jpg'
+    }, {
+      title: 'Grand Knight',
+      name: 'George Olson',
+      picture: 'grandknight.jpg'
+    }, {
+      title: 'Deputy Grand Knight',
+      name: 'Patrick Schmidt',
+      picture: 'deputygrandknight.jpg'
+    }, {
+      title: 'Chancellor',
+      name: 'Steve Gertel',
+      picture: 'chancellor.jpg'
+    }, {
+      title: 'Recorder',
+      name: 'Michael Pascuzzi',
+      picture: 'recorder.jpg'
+    }, {
+      title: 'Financial Secretary',
+      name: 'Yebrail Brennan',
+      picture: 'financialsecretary.jpg'
+    }, {
+      title: 'Treasurer',
+      name: 'Patrick Hayes',
+      picture: 'treasurer.jpg'
+    }, {
+      title: 'Lecturer',
+      name: 'John Rice',
+      picture: 'lecturer.png'
+    }, {
+      title: 'Advocate',
+      name: 'Kraig Johnson',
+      picture: 'advocate.jpg'
+    }, {
+      title: 'Warden',
+      name: 'Jim Fields',
+      picture: 'warden.jpg'
+    }, {
+      title: 'Inside Guard',
+      name: 'Ralph Briscoe',
+      picture: 'insideGuard.jpg'
+    }, {
+      title: 'Outside Guard',
+      name: 'Gary Gagnon',
+      picture: 'outsideGuard.jpg'
+    }, {
+      title: 'One Year Trustee',
+      name: 'Rick Brennan',
+      picture: 'oneYearTrustee.jpg'
+    }, {
+      title: 'Two Year Trustee',
+      name: 'Bill Simpson',
+      picture: 'twoYearTrustee.jpg'
+    }, {
+      title: 'Three Year Trustee',
+      name: 'Ed Gold',
+      picture: 'threeYearTrustee.jpg'
+    }, ]
+  });
 };
